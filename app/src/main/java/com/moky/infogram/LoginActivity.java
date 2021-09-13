01b4bd4dce97b764315e79f52b7445685f5967e3
moky1u1 <skro11t@gmail.com>
2021-09-12 20:03:11 -0400

    TextView Clickable

--- app/src/main/java/com/moky/infogram/LoginActivity.java
+++ app/src/main/java/com/moky/infogram/LoginActivity.java
@@ -2,6 +2,7 @@ package com.moky.infogram;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void goCreateAccount(View view){
+        System.out.println("-----------------> Go to create account");
     }
 }
