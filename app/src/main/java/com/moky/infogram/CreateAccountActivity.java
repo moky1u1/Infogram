525636a09354f361932d57c9bee27f9ecd77e0e3
moky1u1 <skro11t@gmail.com>
2021-09-12 21:25:39 -0400

    Toolbaar

--- app/src/main/java/com/moky/infogram/CreateAccountActivity.java
+++ app/src/main/java/com/moky/infogram/CreateAccountActivity.java
@@ -1,6 +1,8 @@
 package com.moky.infogram;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
 import android.os.Bundle;
 
 public class CreateAccountActivity extends AppCompatActivity {
@@ -9,5 +11,15 @@ public class CreateAccountActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+
+        showToolBar(getString(R.string.title_crear_cuenta),true);
+    }
+
+    public void showToolBar(String titulo, boolean botonSubir){
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setTitle(titulo);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(botonSubir);
     }
 }
