7e552e106b3d598b7ff638245c34497b2802ce1a
moky1u1 <skro11t@gmail.com>
2021-09-12 21:45:56 -0400

    Intent

--- app/src/main/java/com/moky/infogram/LoginActivity.java
+++ app/src/main/java/com/moky/infogram/LoginActivity.java
@@ -1,6 +1,8 @@
 package com.moky.infogram;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -13,6 +15,7 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public void goCreateAccount(View view){
-        System.out.println("-----------------> Go to create account");
+        Intent intent = new Intent(this, CreateAccountActivity.class);
+        startActivity(intent);
     }
 }
